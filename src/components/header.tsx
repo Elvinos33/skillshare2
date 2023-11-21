32840a0cd1cd049a37bcdd98af08fde3c77b0736
Jacob Kielland-Hansen <jacob.kielhans@gmail.com>
2023-11-21 14:54:52 +0100

    Ferdig med header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,15 +2,15 @@ import Link from "next/link"
 
 export default function Header() {
     return (
-        <main className="h-[4rem] w-screen flex items-center shadow-md">
+        <main className="h-[7rem] w-screen flex items-center shadow-md">
             <div className="w-1/2 pl-8">
                 <div className="flex bg-background">
-                    <input type="text" className="text-black brightness-95 rounded p-3 bg-background w-full" placeholder="Søk..." />
                 </div>
             </div>
             <div className="w-1/2 flex justify-end mr-4">
-                <Link href="/LoggInn" className="py-2 w-[10rem] bg-primary rounded flex justify-center text-background font-semibold">Logg inn</Link>
-                <button className="ml-8 py-2 w-[10rem] bg-primary rounded flex justify-center text-background font-semibold">Filter</button>
+                <input type="text" className="text-black rounded p-3 flex outline-none items-center border border-primary bg-background" placeholder="Søk..." />
+                <Link href="/LoggInn" className=" ml-8 py-2 w-[10rem] bg-primary rounded flex justify-center text-background font-semibold items-center hover:border hover:border-primary hover:bg-background hover:text-primary">Logg inn</Link>
+                <button className="ml-8 py-2 w-[10rem] bg-primary rounded flex justify-center text-background font-semibold items-center hover:border hover:border-primary hover:bg-background hover:text-primary">Filter</button>
             </div>
         </main>
     )

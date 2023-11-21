b3f920a689819b3ff21718e8e8584f6132fe581d
Elvin Baloku <elvinos33@duck.com>
2023-11-21 15:25:25 +0100

    signinuser

--- firebaseAuth.ts
+++ firebaseAuth.ts
@@ -1,10 +1,21 @@
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 
 const auth = getAuth()
 
 function createUser(email: string, password: string) {
   createUserWithEmailAndPassword(auth, email, password)
     .then ((userCredential) => {
+      const user = userCredential.user;
+      return user
+    })
+    .catch((error) => {
+      return error.message
+    })
+}
+
+function signIn(email: string, password: string) {
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
       const user = userCredential.user;
       return user
     })

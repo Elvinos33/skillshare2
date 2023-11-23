efcb1370eb83ec7ac923fe84168bc337fdb9ecbc
geiteadmiral <silje.hennie.johansen@gmail.com>
2023-11-23 13:07:56 +0100

    login error fungerer fortsatt ikke

--- src/auth/firebaseAuth.ts
+++ src/auth/firebaseAuth.ts
@@ -3,30 +3,25 @@ import app from "@/lib/firebaseConfig";
 
 const auth = getAuth(app);
 
-export function createUser(email: string, password: string): Promise<any> {
-  return createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      const user = userCredential.user;
-      return user;
-    })
-    .catch((error) => {
-      throw error.message;
-    });
+export async function createUser(email: string, password: string): Promise<any> {
+  try {
+    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+    const user = userCredential.user;
+    return user;
+  } catch (error) {
+    throw error.message;
+  }
 }
 
 
-export function signIn(email: string, password: string): Promise<any> {
-  return new Promise((resolve, reject) => {
-    signInWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        const user = userCredential.user;
-        resolve(user); // Resolve the Promise with the user
-      })
-      .catch((error) => {
-        console.error("Firebase Auth Error:", error); // Log the entire error object
-        reject(error); // Reject the Promise with the error object
-      });
-  });
+export async function signIn(email: string, password: string): Promise<any> {
+  try {
+    const userCredential = await signInWithEmailAndPassword(auth, email, password);
+    const user = userCredential.user;
+    return user;
+  } catch (error) {
+    throw error.message;
+  }
 }
 
 

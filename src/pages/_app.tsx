5b386b1015b90a0f9a7b8eb76b398329eda190f2
geiteadmiral <silje.hennie.johansen@gmail.com>
2023-11-24 13:00:44 +0100

    fikset feil

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,16 @@
 import '@/styles/globals.css';
-import { getAuth, onAuthStateChanged } from 'firebase/auth'; 
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import type { AppProps } from 'next/app';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 
+import app from '@/lib/firebaseConfig';
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
 
   useEffect(() => {
     const checkUser = async () => {
-      const auth = getAuth();
+      const auth = getAuth(app);
 
       onAuthStateChanged(auth, (user) => {
         if (!user && router.pathname.startsWith("/backpage")) {

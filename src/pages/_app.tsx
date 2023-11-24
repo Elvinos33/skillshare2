8551100fa8a7c2f84b60bd838d30faa8792a949e
geiteadmiral <silje.hennie.johansen@gmail.com>
2023-11-24 12:52:34 +0100

    krev login for /backpage

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,25 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import { getAuth, onAuthStateChanged } from 'firebase/auth'; 
+import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+
+  useEffect(() => {
+    const checkUser = async () => {
+      const auth = getAuth();
+
+      onAuthStateChanged(auth, (user) => {
+        if (!user && router.pathname.startsWith("/backpage")) {
+          router.push("/");
+        }
+      });
+    };
+
+    checkUser();
+  }, [router.pathname]);
+
+  return <Component {...pageProps} />;
 }

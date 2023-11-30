f3c5bfe323865f8eff0fcf49220da417495926de
geiteadmiral <silje.hennie.johansen@gmail.com>
2023-11-30 12:32:10 +0100

    tom logg placeholder

--- src/pages/backpage/logg.tsx
+++ src/pages/backpage/logg.tsx
@@ -25,7 +25,21 @@ export default function Logg() {
                     <button className="w-full flex flex-start py-4 pl-4 hover:bg-background text-background hover:text-primary">Tilbake</button>
                     <button onClick={handleLogout} className="w-full flex flex-start py-4 pl-4 hover:bg-background text-background hover:text-primary">Logg ut</button>
                 </div>
-                <div className="w-4/5"></div>
+                <div className="w-4/5 p-2 relative">
+          <div className="h-[7rem] w-full flex items-center shadow-md">
+            <div className="w-1/2 pl-8">
+              <div className="flex bg-background text-2xl font-semibold">Logg</div>
+            </div>
+            <div className="w-1/2 flex justify-end mr-4">
+              <input type="text" className="text-black rounded p-3 flex outline-none items-center border border-primary bg-background" placeholder="Søk..."/>
+              <button  className="ml-8 py-2 w-[10rem] bg-primary rounded flex justify-center text-background font-semibold items-center hover:border hover:border-primary hover:bg-background hover:text-primary">Slett logg</button>
+            </div>
+          </div>
+          <div className="mt-2 ml-4">
+            Det er ingenting i loggen din...
+          </div>
+</div>
+
             </main>
         </>
     )
